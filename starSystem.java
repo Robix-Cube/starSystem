40e932cf924e482cc181608b8fc36e78aea3adf8
Robix-Cube <sharp.robertas@gmail.com>
2018-10-09 14:19:41 +0100

    Added 4 planets and the moon. Finished the task

--- starSystem.java
+++ starSystem.java
@@ -1,5 +1,17 @@
 public class starSystem
 {
-	SolarSystem system =new SolarSystem(600, 600);
-	
+	public static void main(String[] args)
+	{
+		SolarSystem system =new SolarSystem(600, 600);
+
+		system.drawSolarObject(0,135,50,"YELLOW");
+		system.drawSolarObject(100,45,10,"GRAY");
+		system.drawSolarObject(130,90,18,"PINK");
+		system.drawSolarObject(230,0,35,"BLUE");
+		system.drawSolarObject(285,200,30,"RED");
+		
+		system.drawSolarObjectAbout(50,195,5,"WHITE",230,0);
+		
+		system.finishedDrawing();
+	}
 }
