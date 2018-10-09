f9ccaadaa7945c402c9e6c79c9c9ae80b8896774
Robix-Cube <sharp.robertas@gmail.com>
2018-10-09 14:55:38 +0100

    added randomised asteroid belt

--- starSystem.java
+++ starSystem.java
@@ -1,8 +1,17 @@
+import java.util.Random;
+
 public class starSystem
 {
+	/*private static int generateRandom(int min, int max)
+	{
+	// max - min + 1 will create a number in the range of min and max, including max. If you don´t want to include it, just delete the +1.
+	// adding min to it will finally create the number in the range between min and max
+	return r.nextInt((max-min)+1) + min;
+	}*/
 	public static void main(String[] args)
 	{
-		SolarSystem system =new SolarSystem(600, 600);
+		SolarSystem system =new SolarSystem(800, 800);
+		Random r = new Random();
 
 		system.drawSolarObject(0,135,50,"YELLOW");
 		system.drawSolarObject(100,45,10,"GRAY");
@@ -11,7 +20,12 @@ public class starSystem
 		system.drawSolarObject(285,200,30,"RED");
 		
 		system.drawSolarObjectAbout(50,195,5,"WHITE",230,0);
+		system.drawSolarObjectAbout(50,345,2,"WHITE",285,200);
+		
+		for (int i=0;i<200;i++)
+			system.drawSolarObject(r.nextInt(50)+320,r.nextInt(360),r.nextInt(6),"WHITE");
 		
 		system.finishedDrawing();
 	}
+	
 }
