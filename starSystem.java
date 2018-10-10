459c9443b27c5b8979f18a3402f7bbb492bca7ed
Robix-Cube <sharp.robertas@gmail.com>
2018-10-10 13:41:36 +0100

    made planets and moons rotate

--- starSystem.java
+++ starSystem.java
@@ -3,26 +3,33 @@ public class starSystem
 {
 	public static void main(String[] args)
 	{
-		//initilise window and RNG
+		//initilise window, RNG and speed values
 		SolarSystem system =new SolarSystem(850, 850);
-		Random r = new Random();
-
+		Random r = new Random(1);
+		int planetSpeed=1;
+		int moonSpeed=1;
+		
+		while(true)
+		{
 		//draw sun and planets
 		system.drawSolarObject(0,135,50,"YELLOW");
-		system.drawSolarObject(100,45,10,"GRAY");
-		system.drawSolarObject(130,90,18,"PINK");
-		system.drawSolarObject(230,0,35,"BLUE");
-		system.drawSolarObject(285,200,30,"RED");
+		system.drawSolarObject(100,45+planetSpeed,10,"GRAY");
+		system.drawSolarObject(130,90+planetSpeed,18,"PINK");
+		system.drawSolarObject(230,0+planetSpeed,35,"BLUE");
+		system.drawSolarObject(285,200+planetSpeed,30,"RED");
 		
 		//draw moons
-		system.drawSolarObjectAbout(50,195,5,"WHITE",230,0);
-		system.drawSolarObjectAbout(50,345,2,"WHITE",285,200);
+		system.drawSolarObjectAbout(50,195+planetSpeed,5,"WHITE",230,0+moonSpeed);
+		system.drawSolarObjectAbout(50,345+planetSpeed,2,"WHITE",285,200+moonSpeed);
 		
 		//draw asteroid belt
-		for (int i=0;i<200;i++)
-			system.drawSolarObject(r.nextInt(50)+350,r.nextInt(361),r.nextInt(6),"WHITE");
+		for (int i=0;i<10;i++)
+			system.drawSolarObject(r.nextInt(50)+350,r.nextInt(360),r.nextInt(6),"WHITE");
 		
 		//finalise
 		system.finishedDrawing();
+		planetSpeed+=1;
+		moonSpeed+=1;
+		}
 	}
 }
